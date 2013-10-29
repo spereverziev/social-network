4c479ca2ebe18a0c27dee95d59278acd5ac10044
Stanislav Pereverziev <staspereverzev@gmail.com>
2013-10-29 12:52:18 +0200

    add hibernate util class

--- src/main/java/com/stanislav/pereverziev/socialnetwork/entity/User.java
+++ src/main/java/com/stanislav/pereverziev/socialnetwork/entity/User.java
@@ -1,12 +1,16 @@
 package com.stanislav.pereverziev.socialnetwork.entity;
-
+import javax.persistence.*;
 import java.io.Serializable;
 
 /**
  * User: Stanislav.Pereverziev
  * Date: 10/3/13
  */
+@Entity
+@Table(name="users")
 public class User implements Serializable {
+    @Id
+    @GeneratedValue
     private int id;
     private String login;
     private String password;
@@ -26,6 +30,7 @@ public class User implements Serializable {
 
     public void setId(int id) {
         this.id = id;
+        this.id = id;
     }
 
     public String getLogin() {
@@ -42,5 +47,27 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (id != user.id) return false;
+        if (!login.equals(user.login)) return false;
+        if (!password.equals(user.password)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + login.hashCode();
+        result = 31 * result + password.hashCode();
+        return result;
     }
 }
